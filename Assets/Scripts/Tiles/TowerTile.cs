bd5764c3c9ce1c9dd634201f8c2a91e85a719fa2
Killian <32911550+Meshanda@users.noreply.github.com>
2024-11-08 18:41:51 +0100

    Custom tile

--- Assets/Scripts/Tiles/TowerTile.cs
+++ Assets/Scripts/Tiles/TowerTile.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 
 [CreateAssetMenu(menuName = "CustomTile/TowerTile")]
-public class TowerTile : Tile
+public class TowerTile : CustomTile
 {
-
+    public override void OnTile(Entity entity)
+    {
+        Debug.Log("JE SUIS " + entity.name + " ET JE SUIS SUR " + name);
+    }
 }
