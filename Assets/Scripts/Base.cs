ce22a151a58a3b48e8076c4859caa2382aeae842
Christophe Fracheboud <christophe.fracheboud.rime@gmail.com>
2024-11-10 19:12:46 +0100

    Change The Way The Base Is Corrupt

---
+++ Assets/Scripts/Base.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Base : Corruptible
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Enemy enemy = other.gameObject.GetComponentInParent<Enemy>();
+
+            if (enemy == null) return;
+            
+            Corrupt(enemy.CorruptionRate);
+        
+            // Check if it's game over
+            if (Corruption > MaxCorruption)
+            {
+                // Game Over : Call the event for "Game Over"
+                GameManager.OnGameOver?.Invoke(Enums.GameOverType.BaseDestroyed);
+            }
+            else
+            {
+                // Call something to tell the corruption has changed (To modify camera / etc. )
+                GameManager.OnEnemyHitBase?.Invoke(Corruption / MaxCorruption);
+            }
+        }
+    }
+}
--- Assets/Scripts/Tiles/MainBaseTile.cs
+++ Assets/Scripts/Tiles/MainBaseTile.cs
@@ -3,34 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "CustomTile/MainBaseTile")]
 public class MainBaseTile : CorruptibleTile
 {
-   
     public override void OnTile(Entity entity)
     {
-        if (entity is Enemy enemy)
-        {
-            EnemyTouched(enemy.CorruptionRate);
-        }
+        
     }
-    
-    public void EnemyTouched(float corruption)
-    {
-        Corrupt(corruption);
-        
-        // Modify MainBase Sprite to Show Corruption
-        
-        
-        // Check if it's game over
-        if (Corruption > _maxCorruption)
-        {
-            // Game Over : Call the event for "Game Over"
-            GameManager.OnGameOver?.Invoke(Enums.GameOverType.BaseDestroyed);
-        }
-        else
-        {
-            // Call something to tell the corruption has changed (To modify camera / etc. )
-            GameManager.OnEnemyHitBase?.Invoke(Corruption);
-        }
-    }
-
-
 }
