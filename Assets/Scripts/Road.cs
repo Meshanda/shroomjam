128a0a597027ea2da75f08273d9e7b21b1598692
Christophe Fracheboud <christophe.fracheboud.rime@gmail.com>
2024-11-11 15:31:08 +0100

    To Test

--- Assets/Scripts/Road.cs
+++ Assets/Scripts/Road.cs
@@ -3,8 +3,16 @@ using UnityEngine;
 
 public class Road : Corruptible
 {
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+
     public void ChangeCorruption(float value)
     {
         Corrupt(value);
+    }
+    
+    public void ChangeSprite(Sprite sprite)
+    {
+        Debug.Log(sprite);
+        _spriteRenderer.sprite = sprite;
     }
 }
