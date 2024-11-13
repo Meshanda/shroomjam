8ebe3d35ae98de2ec2239a344edfec85b9b3818a
Killian <32911550+Meshanda@users.noreply.github.com>
2024-11-13 17:17:48 +0100

    Character god mode on game over

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -5,14 +5,23 @@ public class Character : Corruptible
     private void OnEnable()
     {
         GameManager.OnGameWin += OnGameWin;
+        GameManager.OnGameOver += OnGameOver;
     }
+
     
+
     private void OnDisable()
     {
         GameManager.OnGameWin -= OnGameWin;
+        GameManager.OnGameOver -= OnGameOver;
     }
 
     private void OnGameWin()
+    {
+        SetGodMode();
+    }
+    
+    private void OnGameOver(Enums.GameOverType obj)
     {
         SetGodMode();
     }
---
+++ Assets/Scripts/UI/TowerRangeDisplay.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TowerRangeDisplay : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Collider2D _attackCollider;
+    [SerializeField] private GameObject _rangeDisplay;
+
+    private void Awake()
+    {
+        _attackCollider = GetComponent<Collider2D>();
+        _rangeDisplay.SetActive(false);
+    }
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        DisplayRange(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        DisplayRange(false);
+    }
+
+    private void DisplayRange(bool b)
+    {
+        _rangeDisplay.SetActive(b);
+    }
+}
