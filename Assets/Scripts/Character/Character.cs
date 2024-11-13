c1105cb338435a7af9a2424ed083f8b8c135a41e
Killian <32911550+Meshanda@users.noreply.github.com>
2024-11-13 16:09:14 +0100

    God(e) mode

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -1,4 +1,25 @@
+using System;
+
 public class Character : Corruptible
 {
+    private void OnEnable()
+    {
+        GameManager.OnGameWin += OnGameWin;
+    }
     
+    private void OnDisable()
+    {
+        GameManager.OnGameWin -= OnGameWin;
+    }
+
+    private void OnGameWin()
+    {
+        SetGodMode();
+    }
+
+    private void SetGodMode()
+    {
+        DeCorrupt(float.MaxValue);
+        CorruptionRate = 0;
+    }
 }
